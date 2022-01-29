9e23f59c34e0c3ec0510174094730e812b230df7
ali1saleem1hasan <ali1saleem1hasan@gmail.com>
2022-01-29 11:22:56 +0200

    initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+
+      {/* TicTacToe */}
+      {/* has 3 Rows */}
+      {/* has 3 Blocks */}
+    </div>
+  );
+}
+
+export default App;
